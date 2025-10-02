1105f2b23d5d0e6002e031ba38b8af8f4f3aea4c
yujining3827 <j_yujin_swu22@naver.com>
2025-10-03 01:58:01 +0900

    feat : /workspace/create 구현

--- src/main/java/com/pres/pres_server/controller/WorkspaceController.java
+++ src/main/java/com/pres/pres_server/controller/WorkspaceController.java
@@ -1,12 +1,13 @@
 package com.pres.pres_server.controller;
 
+import com.pres.pres_server.domain.User;
 import com.pres.pres_server.dto.Workspace.WorkspaceInfoDTO;
+import com.pres.pres_server.dto.Workspace.WorkspaceRequest;
 import com.pres.pres_server.service.WorkspaceService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/workspace")
@@ -14,6 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class WorkspaceController {
 
     private final WorkspaceService workSpaceService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createWorkspace(
+            @RequestBody WorkspaceRequest request,
+            @AuthenticationPrincipal User user) {
+        System.out.println("로그인한 유저 이메일: " + user.getEmail());
+        return ResponseEntity.ok(workSpaceService.createWorkspace(request, user));
+    }
 
     @GetMapping("/{workspaceId}/info")
     public WorkspaceInfoDTO getWorkspaceInfo(@PathVariable Long workspaceId) {
