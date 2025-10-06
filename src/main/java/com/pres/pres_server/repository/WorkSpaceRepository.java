222e552e6c2f38eb6ccc86c3724c07a174681ac3
yujining3827 <j_yujin_swu22@naver.com>
2025-10-06 20:10:51 +0900

    fix: rename WorkSpace* files to Workspace*

--- src/main/java/com/pres/pres_server/repository/WorkSpaceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pres.pres_server.repository;
-
-import com.pres.pres_server.domain.TeamMember;
-import com.pres.pres_server.domain.WorkSpace;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface WorkSpaceRepository extends JpaRepository<WorkSpace, Long> {}
