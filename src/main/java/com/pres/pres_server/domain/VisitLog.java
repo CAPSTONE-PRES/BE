70819fa00e30870b1e61fcc1507ab89adc8f8ef1
yujining3827 <j_yujin_swu22@naver.com>
2025-10-06 20:34:38 +0900

    feat : VisitLog 추가

---
+++ src/main/java/com/pres/pres_server/domain/VisitLog.java
@@ -0,0 +1,38 @@
+package com.pres.pres_server.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "visit_logs")
+@Getter
+@Setter
+public class VisitLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 누가 방문했는지
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // 어떤 워크스페이스를 방문했는지
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workspace_id", nullable = false)
+    private WorkSpace workspace;
+
+    // 어떤 프로젝트를 방문했는지 (없을 수도 있으니 nullable 허용)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    private LocalDateTime visitedAt;
+
+    @PrePersist
+    public void prePersist() {
+        visitedAt = LocalDateTime.now();
+    }
+}
