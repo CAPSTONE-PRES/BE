e0327044e6db93202ca4eb7580f833bdf840d8e5
jjjjhs <fnskquf@gmail.com>
2025-10-19 03:43:35 +0900

    add: qr 의존성, dto 필드 추가

--- src/main/java/com/pres/pres_server/dto/file/CueSlideDto.java
+++ src/main/java/com/pres/pres_server/dto/file/CueSlideDto.java
@@ -11,4 +11,7 @@ public class CueSlideDto {
     private int slideNumber;
     private List<CueBasicDto> basic; // BASIC 섹션들
     private String advanced;             // ADVANCED 텍스트
+
+    private String qrSlug;
+    private String qrUrl;
 }
