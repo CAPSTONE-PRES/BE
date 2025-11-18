b153424f74a14172c48921dbbc76a2b75b4e95a5
jjjjhs <fnskquf@gmail.com>
2025-11-18 11:56:05 +0900

    fix: CueSlide,Issue Dto 필드 수정

--- src/main/java/com/pres/pres_server/dto/file/CueSlideDto.java
+++ src/main/java/com/pres/pres_server/dto/file/CueSlideDto.java
@@ -8,12 +8,15 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Builder
-@Data @NoArgsConstructor
+@Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class CueSlideDto {
     private int slideNumber;
     private List<CueBasicDto> basic; // BASIC 섹션들
-    private List<CueAdvancedDto> advanced;             // ADVANCED 섹션들
-    private String qrSlug; //NULLABLE
-    private String qrUrl; //NULLABLE
+    private List<CueAdvancedDto> advanced; // ADVANCED 섹션들
+    private String qrSlug; // NULLABLE
+    private String qrUrl; // NULLABLE
+    private String prevSlug; // nullable - 이전 슬라이드의 qrSlug
+    private String nextSlug; // nullable - 이후 슬라이드의 qrSlug
 }
--- src/main/java/com/pres/pres_server/dto/practice/IssueDto.java
+++ src/main/java/com/pres/pres_server/dto/practice/IssueDto.java
@@ -1,23 +1,32 @@
 package com.pres.pres_server.dto.practice;
 
 import java.util.Map;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+// null이면 json에 표시되지 않음
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 public class IssueDto {
     private String issueType;
     private Integer fillerCount;
     private Map<String, Integer> fillerDetail;
+    private Integer silenceCount;
     private Integer repeatCount;
     private Map<String, Integer> repeatDetail;
     private Integer errorCount;
     private Integer spmUser;
     private Integer spmAverage;
     private String comment;
+    // 하이라이트용 오프셋 리스트 (begin/end: 슬라이드 텍스트 기준 인덱스)
+    private List<OffsetDto> offsets;
 
 }
